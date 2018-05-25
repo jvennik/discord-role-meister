b88b980581fb974849cfa24825baf5c74137d8b2
Jos Vennik <jos@silveregg.co.jp>
2018-05-25 17:19:10 +0900

    utils/permissions: Fixed issue where member could be null and roles could not be accessed

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -5,7 +5,7 @@ async function isMod(msg) {
   let hasRole = false;
   config.authorizedRoles.forEach(roleName => {
     const role = msg.guild.roles.find('name', roleName);
-    if (msg.member.roles.has(role)) {
+    if (msg.member && msg.member.roles.has(role)) {
       hasRole = true;
     }
   });
