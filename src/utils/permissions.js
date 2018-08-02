22b78f94ea792c77b13bd11e63e2c5f0ad965b8f
Jos Vennik <jos@silveregg.co.jp>
2018-08-02 09:05:14 +0900

    Resolved issue where msg.member being null triggered a TypeError

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -3,12 +3,14 @@ const config = require('config');
 export const isMod =
 async function isMod(msg) {
   let hasRole = false;
-  config.authorizedRoles.forEach(roleName => {
-    const role = msg.guild.roles.find('name', roleName);
-    if (msg.member && msg.member.roles.has(role)) {
-      hasRole = true;
-    }
-  });
+  if (msg.member) {
+    config.authorizedRoles.forEach(roleName => {
+      const role = msg.guild.roles.find('name', roleName);
+      if (msg.member.roles.has(role)) {
+        hasRole = true;
+      }
+    });
+  }
   return hasRole;
 };
 
