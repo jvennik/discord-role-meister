05d994ce3f2377b6edcb37c0070bd23e57491cf4
Jos Vennik <josvennik@gmail.com>
2020-08-30 08:26:26 +0900

    EmojiCollector now automatically rebinds periodically

--- src/utils/collectors/setup-add-emoji-collector.js
+++ src/utils/collectors/setup-add-emoji-collector.js
@@ -1,14 +1,24 @@
-import { grantRole } from '../roles/grant-role';
+import { grantRole } from "../roles/grant-role";
 
-export const setupAddEmojiCollector =
-async function setupAddEmojiCollector(msg, filter) {
-  const collector = msg.createReactionCollector(filter);
-  collector.on('collect', reaction => {
+export const setupAddEmojiCollector = async function setupAddEmojiCollector(
+  msg,
+  filter
+) {
+  const collector = msg.createReactionCollector(filter, {
+    time: 1000 * 60 * 30,
+    dispose: true
+  });
+
+  collector.on("collect", reaction => {
     reaction.users.forEach(user => {
       if (!user.bot) {
         grantRole(msg, reaction.emoji.name, user);
       }
     });
+  });
+
+  collector.on("end", async () => {
+    await setupAddEmojiCollector(msg, filter);
   });
 };
 
